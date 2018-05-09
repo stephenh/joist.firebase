23bbe449ec6bbb49b5b2bfe6ac81acd860405038
Stephen Haberman <stephen.haberman@gmail.com>
2018-05-08 20:36:21 -0500

    Extract hasOne to its own file.

--- src/decorators/hasOne.ts
+++ src/decorators/hasOne.ts
@@ -1,18 +1,17 @@
 
 import { ModelClass } from '@src/model';
-import { PrimitiveProperty, Schema } from '@src/schema';
-import 'reflect-metadata';
+import { Schema } from '@src/schema';
+import { HasOneProperty } from '@src/schema/HasOneProperty';
 import { log as parentLog } from './';
 
 const log = parentLog.child('property');
 
-/** Accepts/returns primitive values, e.g strings, numbers, etc. */
-export function property(): PropertyDecorator {
+/** Accepts/returns a model (or model promise) and store it as the key. */
+export function hasOne(parentClass: ModelClass<any>): PropertyDecorator {
   return (proto: Object, name: string | symbol) => {
-    const type: string = Reflect.getMetadata('design:type', proto, name).name;
-    const prop = new PrimitiveProperty(name, type);
+    const prop = new HasOneProperty(name, parentClass);
     Schema.getSchema(proto).properties.push(prop);
-    log('Defining %o on %o', prop, proto);
+    log('Defining %s on %o', prop, proto);
     Object.defineProperty(proto, name, {
       get: function (): any {
         // tslint:disable-next-line:no-invalid-this
--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,3 +3,4 @@ import { Log, log as parentLog } from '../';
 export const log = parentLog.child('model');
 
 export * from './property';
+export * from './hasOne';
